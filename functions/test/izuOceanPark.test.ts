d8a7aac419478a5d90594d659dba75cb23e9a919
t.mato <t.mato@i3design.co.jp>
2020-04-05 22:16:17 +0900

    20200405のテストケース追加

--- functions/test/izuOceanPark.test.ts
+++ functions/test/izuOceanPark.test.ts
@@ -4,3 +4,8 @@ test('20191001の透明度取得', () => {
     const clearness = izuOceanParkService.extract('https://crystal-clear-ocean.web.app/test/izuOceanPark/20191001.html');
     expect(clearness).toStrictEqual({"values": [10, 15],"measured_at": "2020-10-01T08:00:00+09:00"})
 })
+
+test('20200405の透明度取得', () => {
+    const clearness = izuOceanParkService.extract('https://crystal-clear-ocean.web.app/test/izuOceanPark/20200405.html');
+    expect(clearness).toStrictEqual({"values": [10],"measured_at": "2020-04-05T07:30:00+09:00"})
+})
