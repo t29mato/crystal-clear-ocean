c76550a712c777e7b37fe89eec6ee254f822f7b9
t.mato <t.mato@i3design.co.jp>
2020-04-07 00:51:15 +0900

    タイムゾーンがずれるため

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions'
 import * as izuOceanParkService  from './izuOceanParkService'
 
-export const izuOceanPark = functions.https.onRequest((request, response) => {
+export const izuOceanPark = functions.region('asia-northeast1').https.onRequest((request, response) => {
     izuOceanParkService.crawl()
     response.send("Hello from Firebase!");
     return null
